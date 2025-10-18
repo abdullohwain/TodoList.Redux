dd45f2b00effd2cb168a1f7c451cbabdc45458fd
Abdulloh <abdullohyoqubjonov110@gmail.com>
2025-10-18 16:02:42 +0500

    imtihon loyihasi

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteTodo } from "../files/todos/todoSlice";
-import { changeDone } from "../files/todos/todoSlice"
+import { deleteTodo, changeDone } from "../files/todos/todoSlice";
 import { FaPencilAlt } from "react-icons/fa";
 import { RiDeleteBin6Fill } from "react-icons/ri";
-
 
 function TodoList() {
   const todos = useSelector((state) => state.todos.todoList);
@@ -12,18 +10,20 @@ function TodoList() {
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-2xl">
-      <h2 className="text-2xl  text-center mb-4 text-black">
-        Todo List
-      </h2>
+      <h2 className="text-2xl text-center mb-4 text-black">Todo List</h2>
 
-        <ul className="space-y-3">
-          {todos.map((todo) => (
-            <li
-              key={todo.id}
-              className="flex  justify-between items-center bg-gray-100 hover:bg-gray-200 transition-all px-4 py-2 rounded-lg"
+      <ul className="space-y-3">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="flex justify-between items-center bg-gray-100 hover:bg-gray-200 transition-all px-4 py-2 rounded-lg"
+          >
+            <span
+              className={`text-gray-700 ${todo.done ? "line-through text-gray-400" : ""}`}
             >
-              <span className="text-gray-700">{todo.text}</span>
-              <div> 
+              {todo.text}
+            </span>
+            <div>
               <button
                 onClick={() => dispatch(changeDone(todo.id))}
                 type="button"
@@ -33,15 +33,15 @@ function TodoList() {
               </button>
               <button
                 onClick={() => dispatch(deleteTodo(todo.id))}
-                type="button" 
+                type="button"
                 className="p-2 rounded-md bg-red-300 hover:bg-red-500 text-black transition"
               >
-                <RiDeleteBin6Fill/>
+                <RiDeleteBin6Fill />
               </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
